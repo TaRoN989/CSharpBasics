1bf330822af480bd818cfa32592c4788b8b8e390
Taron Tevanyan <tevanyantaron6@gmail.com>
2024-08-02 16:26:14 +0400

    Add kelvin system

--- CSharpBasics/Temp-convertor/Program.cs
+++ CSharpBasics/Temp-convertor/Program.cs
@@ -12,69 +12,80 @@ namespace Temp_convertor
         {
             Console.WriteLine("Temperature Convertor");
 
+            Console.WriteLine("Choose converting type:");
+            Console.WriteLine("1 - Celsius to Fahrenheit");
+            Console.WriteLine("2 - Fahrenheit to Celsius");
+            Console.WriteLine("3 - Celsius to Kelvin");
+            Console.WriteLine("4 - Kelvin to Celsius");
+            Console.WriteLine("5 - Fahrenheit to Kelvin");
+            Console.WriteLine("6 - Kelvin to Fahrenheit");
+            Console.Write("Enter your choice: ");
+            char unit1 = Char.ToUpper(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+
             Console.Write("Enter the temperature: ");
             double temperature = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter the unit to convert from (C or F): ");
-            char Unit1 = Char.ToUpper(Console.ReadKey().KeyChar);
-            Console.WriteLine();
-
-            Console.Write("Enter the unit to convert to (C or F): ");
-            char Unit2 = Char.ToUpper(Console.ReadKey().KeyChar);
-            Console.WriteLine();
-
-            double convertedTemperature = ConvertTemperature(temperature, Unit1, Unit2);
+            double convertedTemperature = ConvertTemperature(temperature, unit1);
 
             if (!double.IsNaN(convertedTemperature))
             {
-                Console.WriteLine($"The converted temperature is: {convertedTemperature} {Unit2}");
+                Console.WriteLine($"The converted temperature is: {convertedTemperature}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid unit entered.");
             }
 
             Console.WriteLine("Program finished");
             Console.ReadKey();
         }
-
-        static double ConvertTemperature(double temperature, char Unit1, char Unit2)
+        static double ConvertTemperature(double temperature, char unit1)
         {
-            double result = temperature;
-
-            if (Unit1 == 'C')
+            switch (unit1)
             {
-                result = temperature;
-            }
-            else if (Unit1 == 'F')
-            {
-                result = FahrenheitToCelsius(temperature);
-            }
-            else
-            {
-                Console.WriteLine("Invalid unit to convert from.");
-                return double.NaN;
-            }
-
-            if (Unit2 == 'C')
-            {
-                return result;
-            }
-            else if (Unit2 == 'F')
-            {
-                return CelsiusToFahrenheit(result);
-            }
-            else
-            {
-                Console.WriteLine("Invalid unit to convert to.");
-                return double.NaN;
+                case '1':
+                    return CelsiusToFahrenheit(temperature);
+                case '2':
+                    return FahrenheitToCelsius(temperature);
+                case '3':
+                    return CelsiusToKelvin(temperature);
+                case '4':
+                    return KelvinToCelsius(temperature);
+                case '5':
+                    return FahrenheitToKelvin(temperature);
+                case '6':
+                    return KelvinToFahrenheit(temperature);
+                default:
+                    return double.NaN;
             }
         }
-
         static double FahrenheitToCelsius(double fahrenheit)
         {
             return (fahrenheit - 32) * 5 / 9;
         }
-
         static double CelsiusToFahrenheit(double celsius)
         {
             return (celsius * 9 / 5) + 32;
         }
+        static double CelsiusToKelvin(double celsius)
+        {
+            return celsius + 273.15;
+        }
+        static double KelvinToCelsius(double kelvin)
+        {
+            return kelvin - 273.15;
+        }
+        static double FahrenheitToKelvin(double fahrenheit)
+        {
+            double celsius = FahrenheitToCelsius(fahrenheit);
+            return CelsiusToKelvin(celsius);
+        }
+        static double KelvinToFahrenheit(double kelvin)
+        {
+            double celsius = KelvinToCelsius(kelvin);
+            return CelsiusToFahrenheit(celsius);
+        }
     }
 }
+
